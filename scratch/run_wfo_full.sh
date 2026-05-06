a3cf6fb8df9d6d87794fa7b336f01a33a7a55efc
BColladoT <brian.collado7@gmail.com>
2026-05-06 10:40:23 +0200

    chore: gitignore scratch/ exploratory scripts

--- scratch/run_wfo_full.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-cd /mnt/c/quant_trading
-source venv_wsl/bin/activate
-export PYTHONPATH=/mnt/c/quant_trading/src
-setsid python -u src/scripts/train_wfo.py > /tmp/wfo_full_train.log 2>&1 &
-echo "PID: $!"
-disown $!
--- scratch/run_wfo_quick.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-cd /mnt/c/quant_trading
-source venv_wsl/bin/activate
-export PYTHONPATH=/mnt/c/quant_trading/src
-python src/scripts/train_wfo_quick_test.py > /tmp/wfo_quicktest.log 2>&1
-echo "EXIT_CODE: $?" >> /tmp/wfo_quicktest.log
